0f0fa98293f351714180a5119c356763561d742a
Ryszard Plewnia <hello@erys.io>
2021-05-05 19:50:14 +0200

    Remove App test file

--- src/modules/App/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders app placeholder", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/_APP_/i);
-  expect(linkElement).toBeInTheDocument();
-});
